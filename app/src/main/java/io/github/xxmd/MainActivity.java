19884db2095862ab3eedbdb9cbdf6a14d6ff1d75
GMJ <410515742@qq.com>
2024-02-03 17:28:19 +0800

    V1.0.3

--- app/src/main/java/io/github/xxmd/MainActivity.java
+++ app/src/main/java/io/github/xxmd/MainActivity.java
@@ -25,8 +25,6 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         bindEvent();
-        String videoUrl = "https://vdept3.bdstatic.com/mda-peb81ifiai953fsz/sc/cae_h264/1683945818917238364/mda-peb81ifiai953fsz.mp4?v_from_s=hkapp-haokan-nanjing&auth_key=1706620064-0-0-5833b88fe12426899f78e5234f3e6782&bcevod_channel=searchbox_feed&cr=2&cd=0&pd=1&pt=3&logid=0464688034&vid=9029381628758960723&klogid=0464688034&abtest=116096_1-115667_1";
-        FilePreviewer.previewFile(MainActivity.this, videoUrl);
     }
 
     private void bindEvent() {
--- mylibrary/src/main/java/io/github/xxmd/VideoPreviewActivity.java
+++ mylibrary/src/main/java/io/github/xxmd/VideoPreviewActivity.java
@@ -1,31 +1,10 @@
 package io.github.xxmd;
 
-import android.content.pm.ActivityInfo;
-import android.media.MediaPlayer;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.PersistableBundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-
 import androidx.appcompat.widget.Toolbar;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
 import androidx.viewbinding.ViewBinding;
 
-import com.bumptech.glide.Glide;
-
-import java.util.concurrent.TimeUnit;
-
-import cn.jzvd.JZDataSource;
-import cn.jzvd.JzvdStd;
+import cn.jzvd.JZVideoPlayerStandard;
 import io.github.xxmd.databinding.FpActivityVideoPreviewBinding;
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class VideoPreviewActivity extends FilePreviewActivity {
     private FpActivityVideoPreviewBinding binding;
@@ -33,9 +12,9 @@ public class VideoPreviewActivity extends FilePreviewActivity {
     @Override
     public void initView() {
         super.initView();
-        loadCover();
-        JZDataSource jzDataSource = new JZDataSource(filePath, "");
-        binding.jzVideo.setUp(jzDataSource, JzvdStd.SCREEN_NORMAL);
+//        loadCover();
+        binding.jzVideo.setUp(filePath,  JZVideoPlayerStandard.SCREEN_WINDOW_NORMAL, "");
+        binding.jzVideo.startVideo();
     }
 
     @Override
@@ -66,10 +45,10 @@ public class VideoPreviewActivity extends FilePreviewActivity {
     }
 
     private void loadCover() {
-        binding.jzVideo.posterImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        Glide.with(this)
-                .load(filePath)
-                .placeholder(R.drawable.image_placeholder)
-                .into(binding.jzVideo.posterImageView);
+//        binding.jzVideo.posterImageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        Glide.with(this)
+//                .load(filePath)
+//                .placeholder(R.drawable.image_placeholder)
+//                .into(binding.jzVideo.posterImageView);
     }
 }
